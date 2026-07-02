c290b61e61e3942290bd4ffefb9a9e70054a28c0
Sandra B <sandrabeena138@gmail.com>
2026-07-02 20:25:59 +0530

    Add files via upload

---
+++ base2.py
@@ -0,0 +1,20 @@
+#Entering multiple i/p
+'''
+a,b = map(int, input("Enter value for a and b:").split()) 
+print("a =",a)
+print("b =",b)
+print("Swapped value:")
+a,b=b,a
+print("a =",a)
+print("b =",b)
+'''
+
+#Seperator
+'''
+a,b,c = map(int, input("Enter 3 values:").split())
+print(a,b,c,sep="-")
+'''
+
+#Type conversion
+a = 2.5
+print(int(a))
