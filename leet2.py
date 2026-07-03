90ba829fe1f25a460346b66b628ca3f838abfeba
Sandra B <sandrabeena138@gmail.com>
2026-07-03 20:32:47 +0530

    Add files via upload

---
+++ leet2.py
@@ -0,0 +1,29 @@
+'''
+integers to roman number
+'''
+class Solution(object):
+    def romanToInt(self, s):
+
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        total = 0
+        i = 0
+
+        while i < len(s):
+
+            if i < len(s)-1 and roman[s[i]] < roman[s[i+1]]:
+                total = total + roman[s[i+1]] - roman[s[i]]
+                i = i + 2
+            else:
+                total = total + roman[s[i]]
+                i = i + 1
+
+        return total
