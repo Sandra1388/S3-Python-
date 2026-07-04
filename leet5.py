e1ce875166a52cd16ef9ec21e57d2e8147e530a4
Sandra B <sandrabeena138@gmail.com>
2026-07-04 21:02:35 +0530

    Add files via upload

---
+++ leet5.py
@@ -0,0 +1,9 @@
+'''
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+'''
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        nums1[m:]=nums2
+        nums1.sort()
