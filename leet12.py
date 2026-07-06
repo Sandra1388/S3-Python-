ccfc80538d0e7d04948b932a04120a5a90a836f4
Sandra B <sandrabeena138@gmail.com>
2026-07-06 19:27:08 +0530

    Add files via upload

---
+++ leet12.py
@@ -0,0 +1,16 @@
+'''
+Given two strings s and t, return true if t is an anagram of s,
+and false otherwise.
+'''
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) == len(t):
+            if sorted(s) == sorted(t):
+                return True
+        return False
