90b04b10de1fa47a94ec40778d07a6aae3bde94d
Sandra B <sandrabeena138@gmail.com>
2026-07-06 19:26:13 +0530

    Add files via upload

---
+++ leet10.py
@@ -0,0 +1,18 @@
+'''
+Remove duplicate elements from linked list
+'''
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        current = head
+
+        while current is not None and current.next is not None:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
+            
