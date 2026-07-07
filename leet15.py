e0239ac0fab656d96aa7720e218b521fe92a15ab
Sandra B <sandrabeena138@gmail.com>
2026-07-07 20:24:08 +0530

    Add files via upload

---
+++ leet15.py
@@ -0,0 +1,15 @@
+'''
+Given a string s consisting of words and spaces,
+return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+'''
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words = s.split()
+        l = len(words[-1])
+        return l
