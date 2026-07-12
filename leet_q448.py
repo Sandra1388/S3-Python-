8495028cf84ae02596b3cbe74c617bfb4a788d7b
Sandra B <sandrabeena138@gmail.com>
2026-07-12 20:24:08 +0530

    Add files via upload

---
+++ leet_q448.py
@@ -0,0 +1,20 @@
+'''
+Given an array nums of n integers where nums[i] is in the range
+[1, n], return an array of all the integers in the range [1, n]
+that do not appear in nums.
+
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+'''
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        s = []
+        for i in range(1,len(nums)+1):
+           if i not in nums:
+            s.append(i)
+        return s
