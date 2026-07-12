ebcf3b9f55eddd9c0cba8058745ad5fc4e2791b2
Sandra B <sandrabeena138@gmail.com>
2026-07-12 19:37:57 +0530

    Add files via upload

---
+++ leet_q26.py
@@ -0,0 +1,22 @@
+'''
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2,
+with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence
+they are underscores).
+'''
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        k = 1
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
