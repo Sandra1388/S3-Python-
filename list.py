e93f90b55acc86a7d7f4883ccc2512fb63abf40d
Sandra B <sandrabeena138@gmail.com>
2026-07-13 20:15:20 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,59 @@
+#LIST
+
+# Q1) CREATE A SIMPLE LIST
+nums = [20,50,30,40]
+
+#print list element
+'''
+print(nums)
+'''
+
+#print single element
+'''
+print(nums[0])
+print(nums[1])
+'''
+
+#Modify list
+'''
+nums[1] = 1
+print(nums)
+'''
+
+#Append element to list
+'''
+nums.append(50)
+print(nums)
+'''
+
+#Pop list elements one by one
+'''
+print(nums)
+nums.pop()
+print(nums)
+'''
+
+#Inserting to a specific position
+'''
+nums.insert(2,3) #insert element 3 in position 2
+print(nums)
+'''
+
+#Remove a particular element
+'''
+print(nums)
+nums.remove(nums[1])
+print(nums)
+'''
+
+#Sort List
+'''
+print(nums)
+nums.sort()
+print(nums)
+'''
+
+#Reverse List Elements
+print(nums)
+nums.reverse()
+print(nums)
