c2a4ff829da3148d0c391d6bce51b5b34a950991
Sandra B <sandrabeena138@gmail.com>
2026-07-15 19:36:30 +0530

    Add files via upload

---
+++ dictionary_base.py
@@ -0,0 +1,24 @@
+#DICTIONARY
+
+#Create a dictionary
+
+student = {
+    "name" : "Sandra",
+    "age" : 22,
+    "course" : "MCA"
+    }
+'''
+print(student["name"])
+print(student["age"])
+print(student["course"])
+'''
+
+#updating values
+'''
+student["name"] = "Sandraa"
+print(student)
+'''
+
+#Adding new Keys
+student["College"] = "RCSS"
+print(student)
