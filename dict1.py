1ff7ac69aa2a4df716077cc03fe615c04091f406
Sandra B <sandrabeena138@gmail.com>
2026-07-15 19:53:09 +0530

    Add files via upload

---
+++ dict1.py
@@ -0,0 +1,21 @@
+student = {
+    "name": "Sandra",
+    "age": 22,
+    "course": "MCA"
+}
+
+#Check if a key is present in dictionary
+'''
+print("name" in student)
+print("college" in student)
+'''
+
+#Loop through dictionary
+
+#print key only
+for key in student:
+    print(key)
+
+#print value only
+for key in student:
+    print(student[key])
