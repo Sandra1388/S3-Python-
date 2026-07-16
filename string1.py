e7eda81d78226979f5292df37e375236ca502d7d
Sandra B <sandrabeena138@gmail.com>
2026-07-16 21:05:58 +0530

    Add files via upload

---
+++ string1.py
@@ -0,0 +1,21 @@
+# STRING SLICING
+
+s = input("Enter a sentence:")
+
+'''
+print(s[1:4])
+print(s[:3])
+print(s[-1])
+print(s[0])
+'''
+
+print("Upper:",s.upper())
+print("Lower:",s.lower())
+print("Strip:",s.strip())
+print("Replace:",s.replace("a","b"))
+#print("split:",s.input().split())
+print("Find:",s.find("a"))
+print("Count:",s.count("a"))
+print("Start with:",s.startswith("ja"))
+print("Ends with:",s.endswith("a"))
+
