82e6460d5ffac0556b577708d1cef18182633d08
Sandra B <sandrabeena138@gmail.com>
2026-08-27 20:25:32 +0530

    new

---
+++ hack4.py
@@ -0,0 +1,15 @@
+'''
+You are given a N X M integer array matrix with space separated elements (N = rows and M = columns).
+Your task is to print the transpose and flatten results.
+'''
+import numpy as np
+
+n, m = map(int, input().split())
+my_array = []
+for i in range(n):
+    row = list(map(int, input().split()))
+    my_array.append(row)
+my_array = np.array(my_array)
+
+print(np.transpose(my_array)) #transpose
+print(my_array.flatten())     #flatten
